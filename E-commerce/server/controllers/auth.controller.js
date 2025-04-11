425fc318d7ec2d2d2cd5cc382ec5c7ec56ded664
nikhil-177 <pn2683249@gmail.com>
2025-04-11 14:03:07 +0530

    hashing and comparing password, function done

--- E-commerce/server/controllers/auth.controller.js
+++ E-commerce/server/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import {User} from '../models/userModel.js'
+import { User } from '../models/userModel.js';
 
-
-export const signupController = async (req,res) => {
-    const {name,email,password,address,city,state,country,zipcode,phone} = req.body
+export const signupController = async (req, res) => {
+  try {
+    const { name, email, password, address, city, state, country, zipcode, phone } = req.body;
     const user = await User.create({
       name,
       email,
@@ -14,6 +14,9 @@ export const signupController = async (req,res) => {
       zipcode,
       phone,
     });
-    if(!user) console.log(user)
-    res.status(201).send({success:true,message:"user created successfully",user})
-}
+
+    res.status(201).send({ success: true, message: 'user created successfully', user });
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+};
--- E-commerce/server/models/userModel.js
+++ E-commerce/server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema(
   {
@@ -42,5 +43,24 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// password hashing
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+
+// password comparing
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export const User = mongoose.model('User', UserSchema);
